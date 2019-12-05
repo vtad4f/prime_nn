69f34286b78e855cef8f2bb88d0479705c1150a2
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-12-05 06:37:34 -0600

    Code cleanup

--- src-py/data.py
+++ src-py/data.py
@@ -13,7 +13,7 @@ class Regex:
    NUMBER = re.compile(r' ([0-9]+) ', re.MULTILINE)
    
    
-def TrainingData(fpath):
+def TrainingData(fpath, limit = None):
    """
       BRIEF  Parse a text file containing prime numbers
    """
@@ -22,9 +22,22 @@ def TrainingData(fpath):
    
    data = SupervisedDataSet(1, 1)
    
-   for n in range(primes[0], primes[0] + 500): # Keep it small for now
-      data.addSample((n,), (1 if n in primes else 0,))
+   end = primes[-1] if (limit is None) else (primes[0] + limit)
+   for n in range(primes[0], end + 1):
+      data.addSample([n], [1 if n in primes else 0])
       
    return data
    
    
+def TestData(fpath, limit = None):
+   """
+      BRIEF  Don't bother with a SupervisedDataSet
+   """
+   with open(fpath, 'r') as f:
+      primes = map(int, Regex.NUMBER.findall(f.read()))
+      
+   end = primes[-1] if (limit is None) else (primes[0] + limit)
+   for n in range(primes[0], end + 1):
+      yield (n, 1 if n in primes else 0)
+      
+      
--- src-py/main.py
+++ src-py/main.py
@@ -1,22 +1,61 @@
 
-from nn2_data import Data, Path
+from data import TrainingData, TestData, Path
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 import sys
 
-n_inputs = 1
-n_hidden = 3
-n_output = 1
 
-nn = buildNetwork(n_inputs, n_hidden, n_output, bias=True)
-
-print("Test: {0}".format(nn.activate([5])))
-sys.stdout.flush()
-
-trainer = BackpropTrainer(nn, TrainingData(Path.FIRST))
-for epoch in range(100):
-   print(trainer.train())
+def Train(nn, data, n_epoch):
+   """
+      BRIEF  Train using the data set
+   """
+   print("Training...")
    sys.stdout.flush()
    
-print("Test: {0}".format(nn.activate([5])))
-sys.stdout.flush()
+   trainer = BackpropTrainer(nn, data)
+   trainer.trainUntilConvergence(maxEpochs=n_epoch)
+   
+   
+def Test(nn, data):
+   """
+      BRIEF  Test against the data set
+   """
+   print("Testing...")
+   sys.stdout.flush()
+   
+   right = 0
+   wrong = 0
+   for (input, output) in data:
+      if nn.activate([input]) > 0:
+         if output > 0:
+            right += 1
+         else:
+            wrong += 1
+      else:
+         if output > 0:
+            wrong += 1
+         else:
+            right += 1
+            
+   print('right, wrong = {0}, {1} = {2:.2f}'.format(right, wrong, right / float(right + wrong)))
+   sys.stdout.flush()
+   
+   
+if __name__ == '__main__':
+   """
+      BRIEF  Main Execution
+   """
+   n_inputs = 1
+   n_hidden = 20
+   n_output = 1
+   
+   data_limit = 500
+   n_epoch    = 100
+   
+   nn = buildNetwork(n_inputs, n_hidden, n_output, bias=True)
+   
+   Train(nn, TrainingData(Path.FIRST, data_limit), n_epoch)
+   Test(nn, TestData(Path.FIRST, data_limit))
+   Test(nn, TestData(Path.SECOND, data_limit))
+   
+   
